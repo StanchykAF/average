9a98bc6be82c29bd1e5438b5ab6327b2c4b1a77a
Nastassia Stanchyk <stanchikaf@gmail.com>
2022-04-07 17:18:15 +0300

    Solved!

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -1,11 +1,29 @@
 package com.epam.rd.autotasks;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Use Scanner methods to read input
+        ArrayList <Integer> sequence = new ArrayList<>();
+        int i = 0;
+
+        do {
+            sequence.add(scanner.nextInt());
+            i++;
+        } while (sequence.get(i - 1) != 0);
+
+        int result = 0;
+
+        for (int j = 0; j < sequence.size() - 1; j++) {
+            result += sequence.get(j);
+        }
+
+        result /= (sequence.size() - 1);
+
+        System.out.println(result);
 
     }
 
